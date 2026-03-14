dd12a330e54796c2b2a48d48498f3f2b83c98039
David Odesomi <davidodesomi@gmail.com>
2026-03-14 17:50:15 +0100

    Update to Choose your own adventure(Beginner Project)

--- beginner/04_choose_your_own_adventure/main.py
+++ beginner/04_choose_your_own_adventure/main.py
@@ -1,1 +1,12 @@
 # 04 Choose Your Own Adventure
+print("Welcome to choose your adventure.")
+def name():
+    user_name = input("what is your name? enter: ")
+    if user_name in ["Mira", "Old Eli", "Raven"]:
+        user_name = input("Names are taken! Pick another name: ")
+    return user_name;
+user_name = name()
+print(f"Meet the characters: \n{user_name.title()} -> you, \nMira -> Your brave friend, \n"
+      f"Old Eli -> A mysterious groundskeeper, \nRaven -> A shady stranger. ")
+
+print("")
