925aaf70150bf9599c711635758c783efe54a059
David Odesomi <davidodesomi@gmail.com>
2026-03-24 18:07:51 +0100

    refactor(structure): :building_construction: move choices print out of conditional branches

--- beginner/03_rock_paper_scissors/main.py
+++ beginner/03_rock_paper_scissors/main.py
@@ -1,19 +1,17 @@
 # 03 Rock Paper Scissors
 import random
 
-computer_choice = random.randint(1,3)
-
 def rock_paper_scissors():
     computer_choice = random.randint(1, 3)
     print('Welcome To Rock Paper Scissors.')
     print('Rock: 1, Paper: 2, Scissors: 3')
     human_choice = None
-    while human_choice not in (1,2,3):
+    while human_choice not in (1, 2, 3):
         try:
-                human_choice = int(input('Enter your choice: '))
+            human_choice = int(input('Enter your choice: '))
         except ValueError:
-             print("⚠️  Please enter a valid number.\n")
-        continue
+            print("⚠️  Please enter a valid number.\n")
+            continue
         
     if human_choice == 1:
         print('Selected Rock!')
@@ -21,18 +19,18 @@ def rock_paper_scissors():
         print("Selected Paper!")
     else:
         print("Selected Scissors")
+
+    print(f"Computer's choice: {computer_choice}, Your choice: {human_choice}" )
     
     if computer_choice == 1 and human_choice == 2:
-            print(f"Computer's choice: {computer_choice}, Your choice: {human_choice}" )
-            print("You win!")
+        print("You win!")
     elif computer_choice == 2 and human_choice == 3:
-        print(f"Computer's choice: {computer_choice}, Your choice: {human_choice}" )
         print("You win!")
     elif computer_choice == 3 and human_choice == 1:
-            print(f"Computer's choice: {computer_choice}, Your choice: {human_choice}" )
-            print("You win!")
+        print("You win!")
+    elif computer_choice == human_choice:
+        print("Its a draw!!")
     else:
-            print(f"Computer's choice: {computer_choice}, Your choice: {human_choice}" )
-            print("You lose!")
+        print("You lose!")
 
 rock_paper_scissors()
