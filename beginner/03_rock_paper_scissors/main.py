658c950e4ecfe7683b4ff05300b8c7ca8bea33b8
David Odesomi <davidodesomi@gmail.com>
2026-03-24 14:40:01 +0100

    refactor(Continued build): :building_construction: Added the comprasion for both sides choice of hand

--- beginner/03_rock_paper_scissors/main.py
+++ beginner/03_rock_paper_scissors/main.py
@@ -7,15 +7,14 @@ def rock_paper_scissors():
     computer_choice = random.randint(1, 3)
     print('Welcome To Rock Paper Scissors.')
     print('Rock: 1, Paper: 2, Scissors: 3')
-    human_choice = int(input('Enter your choice: '))
-    while human_choice != 1 or 2 or 3:
+    human_choice = None
+    while human_choice not in (1,2,3):
         try:
                 human_choice = int(input('Enter your choice: '))
         except ValueError:
              print("⚠️  Please enter a valid number.\n")
         continue
-    
-    
+        
     if human_choice == 1:
         print('Selected Rock!')
     elif human_choice == 2:
@@ -23,5 +22,17 @@ def rock_paper_scissors():
     else:
         print("Selected Scissors")
     
+    if computer_choice == 1 and human_choice == 2:
+            print(f"Computer's choice: {computer_choice}, Your choice: {human_choice}" )
+            print("You win!")
+    elif computer_choice == 2 and human_choice == 3:
+        print(f"Computer's choice: {computer_choice}, Your choice: {human_choice}" )
+        print("You win!")
+    elif computer_choice == 3 and human_choice == 1:
+            print(f"Computer's choice: {computer_choice}, Your choice: {human_choice}" )
+            print("You win!")
+    else:
+            print(f"Computer's choice: {computer_choice}, Your choice: {human_choice}" )
+            print("You lose!")
 
 rock_paper_scissors()
