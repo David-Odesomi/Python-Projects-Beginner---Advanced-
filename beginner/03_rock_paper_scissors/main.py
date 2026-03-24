967af32515201680ada36508fe4d23a07293a98d
David Odesomi <davidodesomi@gmail.com>
2026-03-24 18:13:48 +0100

    refactor(structure): updated comparison and display of choice

--- beginner/03_rock_paper_scissors/main.py
+++ beginner/03_rock_paper_scissors/main.py
@@ -15,18 +15,28 @@ def rock_paper_scissors():
         
     if human_choice == 1:
         print('Selected Rock!')
+        human_choice = "Rock"
     elif human_choice == 2:
         print("Selected Paper!")
+        human_choice = "Paper"
     else:
         print("Selected Scissors")
+        human_choice = "Scissors"
+
+    if computer_choice == 1:
+        computer_choice = "Rock"
+    elif computer_choice == 2:
+        computer_choice = "Paper"
+    else:
+        computer_choice = "Scissors"
 
     print(f"Computer's choice: {computer_choice}, Your choice: {human_choice}" )
     
-    if computer_choice == 1 and human_choice == 2:
+    if computer_choice == "Rock" and human_choice == "Paper":
         print("You win!")
-    elif computer_choice == 2 and human_choice == 3:
+    elif computer_choice == "Paper" and human_choice == "Scissors":
         print("You win!")
-    elif computer_choice == 3 and human_choice == 1:
+    elif computer_choice == "Scissors" and human_choice == "Rock":
         print("You win!")
     elif computer_choice == human_choice:
         print("Its a draw!!")
