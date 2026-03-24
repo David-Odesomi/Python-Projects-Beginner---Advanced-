532df1c5708b2209c40e6d610595daba72594190
David Odesomi <davidodesomi@gmail.com>
2026-03-24 18:55:17 +0100

    fix: FIX: while loop

--- beginner/02_number_guessing_game/main.py
+++ beginner/02_number_guessing_game/main.py
@@ -7,18 +7,19 @@ def guessing_game():
     random_number = random.randint(1, 10)
     player_number = None
 
-    while player_number not in (1, 2, 3, 4, 5, 6, 7, 8, 9, 10,):
+    while player_number != random_number:
         try:
             player_number = int(input("Enter in your guess: "))
+            if player_number + 1 == random_number or player_number - 1 == random_number:
+                print("So close.")
+            else: 
+                print("Try again!")
         except ValueError:
             print("⚠️  Please enter a valid number.\n")
-            continue
 
-    if player_number == random_number:
-        print("You guessed correctly.")
-    elif player_number - 1 == random_number or player_number + 1 == random_number:
-        print("So close.")
-    else:
-        print("You didnt get it.")
+            continue
+    print('You got it!')
+    print(f"The number was {random_number}!")
+        
 
 guessing_game()
