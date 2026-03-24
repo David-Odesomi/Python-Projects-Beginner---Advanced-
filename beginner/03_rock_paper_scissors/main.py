988fac5463fda9ad1c04eda892bf1b64455c472f
David Odesomi <davidodesomi@gmail.com>
2026-03-24 14:00:33 +0100

    feat(structure): :bookmark: Created Basic logic for the rock paper scissors game by including the random lib and using a while loop to check numbers.

--- beginner/03_rock_paper_scissors/main.py
+++ beginner/03_rock_paper_scissors/main.py
@@ -1,10 +1,27 @@
 # 03 Rock Paper Scissors
-import math
-print('Rock, Paper, or Scissors')
-player_selection = input('Make your choice: ').lower
+import random
 
-computer_selection = math.
+computer_choice = random.randint(1,3)
 
-def play ():
-    if player_selection == 'rock' and computer_selection == 1:
-        print
+def rock_paper_scissors():
+    computer_choice = random.randint(1, 3)
+    print('Welcome To Rock Paper Scissors.')
+    print('Rock: 1, Paper: 2, Scissors: 3')
+    human_choice = int(input('Enter your choice: '))
+    while human_choice != 1 or 2 or 3:
+        try:
+                human_choice = int(input('Enter your choice: '))
+        except ValueError:
+             print("⚠️  Please enter a valid number.\n")
+        continue
+    
+    
+    if human_choice == 1:
+        print('Selected Rock!')
+    elif human_choice == 2:
+        print("Selected Paper!")
+    else:
+        print("Selected Scissors")
+    
+
+rock_paper_scissors()
