5784fe4ac54c05eca465aa3bbaaa198a2ce465b7
David Odesomi <davidodesomi@gmail.com>
2026-03-24 18:30:42 +0100

    fix(structure): fixed, while loop print, elif comparison for numbers, input value type.

--- beginner/02_number_guessing_game/main.py
+++ beginner/02_number_guessing_game/main.py
@@ -5,18 +5,20 @@ import random
 
 def guessing_game():
     random_number = random.randint(1, 10)
-    player_number = input("Enter in your guess: ")
+    player_number = None
 
     while player_number not in (1, 2, 3, 4, 5, 6, 7, 8, 9, 10,):
         try:
-            print("Enter a valid Number Bro: ")
+            player_number = int(input("Enter in your guess: "))
         except ValueError:
             print("⚠️  Please enter a valid number.\n")
             continue
 
     if player_number == random_number:
         print("You guessed correctly.")
-    elif player_number - 1 or player_number + 1 == random_number:
+    elif player_number - 1 == random_number or player_number + 1 == random_number:
         print("So close.")
     else:
         print("You didnt get it.")
+
+guessing_game()
