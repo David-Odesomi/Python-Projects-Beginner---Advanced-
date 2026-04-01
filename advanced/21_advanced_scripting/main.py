c7a71f149e30a84b7f9dafd7b60443083aee9038
David Odesomi <davidodesomi@gmail.com>
2026-04-01 10:25:54 +0100

    untrack generated files and editor settings

--- advanced/21_advanced_scripting/main.py
+++ advanced/21_advanced_scripting/main.py
@@ -1,13 +1,18 @@
 # 21 Advanced Scriptingpip3 install requests
 import requests
 from datetime import datetime
-today = datetime.today().strftime("%Y-%m-%d")
 from dotenv import load_dotenv
 import os
+import pandas as pd
+import csv
+
+today = datetime.today().strftime("%Y-%m-%d")
 
 load_dotenv()
 API_KEY = os.getenv("API_KEY")
 BASE_URL = "https://www.alphavantage.co/query"
+
+# print(API_KEY)
 
 ticker = input("Enter stock ticker: ").upper()
 
@@ -37,8 +42,10 @@ if "Time Series (Daily)" not in data:
     print(f"❌ Could not fetch data. Response: {data}")
     exit()
 
+
 time_series = data["Time Series (Daily)"]
-today_data = time_series[today]
+latest_date = list(time_series.keys())[0]
+today_data = time_series[latest_date]
 
 open_price = today_data["1. open"]
 high = today_data["2. high"] 
@@ -47,3 +54,14 @@ close = today_data["4. close"]
 volume = today_data["5. volume"]
 
 print(open_price)
+
+os.makedirs(f"reports/{ticker}", exist_ok=True)
+
+filepath = f"reports/{ticker}/{latest_date}.csv"
+
+with open(filepath , "w", newline="") as f:
+    writer = csv.writer(f)
+    writer.writerow(["date", "open", "high", "low", "close", "volume"])
+    writer.writerow([today, open_price, high, low, close, volume])
+
+print(f"✅ Report saved to {filepath}")
